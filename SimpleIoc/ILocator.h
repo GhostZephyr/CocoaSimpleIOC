783de34d76c6207f0ac2a6994745c8c1be81ab8d
王韧竹 <wangrenzhu@yunfan.com>
2016-02-19 10:34:31 +0800

    合并

---
+++ SimpleIoc/ILocator.h
@@ -0,0 +1,13 @@
+//
+//  ILocator.h
+//  SimpleIoc
+//
+//  Created by qvod on 14-10-10.
+//  Copyright (c) 2014年 YF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ILocator <NSObject>
++(void) initLocator;
+@end
